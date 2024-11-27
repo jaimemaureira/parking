7b2475bcbd9f5a8e9fb2bf05700ceb867d8a6d72
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2024-11-27 12:01:00 -0300

    profile
    se limpia cache y se corrige profile.ts

--- src/app/pages/main/profile/profile.module.ts
+++ src/app/pages/main/profile/profile.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
-import { ProfilePageRoutingModule } from './profile-routing.module';
-
 import { ProfilePage } from './profile.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { ProfilePageRoutingModule } from './profile-routing.module';
 
 @NgModule({
   imports: [
+    ProfilePageRoutingModule,
     CommonModule,
-    FormsModule,
-    IonicModule,
-    ProfilePageRoutingModule
+    SharedModule,   
+    IonicModule,    
+    FormsModule    
   ],
   declarations: [ProfilePage]
 })
